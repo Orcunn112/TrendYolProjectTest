73a5651bab4f9a2b0799467e0ad722fc0cdca6bd
Orcunn112 <orcun.ilgen@gmail.com>
2022-11-01 02:27:18 +0300

    Initial commit

---
+++ src/TestGruppen/Test1.java
@@ -0,0 +1,16 @@
+package TestGruppen;
+
+import Utils.GenelWebDriver;
+import org.testng.annotations.Test;
+
+public class Test1 extends GenelWebDriver {
+
+
+    @Test
+    void test1(){
+        System.out.println("check 1 ");
+    }
+
+
+    }
+
---
+++ src/Utils/BaseStaticDriver.java
@@ -0,0 +1,55 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseStaticDriver {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+
+    static
+    {
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        Duration dr= Duration.ofSeconds(30);
+        driver.manage().timeouts().pageLoadTimeout(dr);
+        driver.manage().timeouts().implicitlyWait(dr);
+        wait=new WebDriverWait(driver,Duration.ofSeconds(30));
+    }
+
+    public static void BekleKapat()
+    {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+
+    public static void Bekle(int saniye)
+    {
+
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
