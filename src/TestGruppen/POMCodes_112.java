742862a9cc22434c56e115b078db423bd438071d
Orcunn112 <orcun.ilgen@gmail.com>
2022-11-13 14:31:59 +0300

    The project was updated completely

---
+++ src/TestGruppen/POMCodes_112.java
@@ -0,0 +1,26 @@
+package TestGruppen;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class POMCodes_112 {
+
+    public POMCodes_112(WebDriver driver) {PageFactory.initElements(driver, this);}
+
+    @FindBy(xpath = "//button[text()='KABUL ET']")
+    public WebElement clickcokieaccept;
+
+    @FindBy(css = "[class='component-list component-small-list']")
+    public WebElement Allimagines;
+
+    @FindBy(xpath = "//*[@class='component-item']")
+    public List<WebElement> counttheelements;
+
+
+
+}
+
--- src/TestGruppen/Test1.java
+++ src/TestGruppen/Test1.java
@@ -1,16 +1,59 @@
 package TestGruppen;
 
 import Utils.GenelWebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class Test1 extends GenelWebDriver {
 
 
-    @Test
-    void test1(){
-        System.out.println("check 1 ");
-    }
+    @Test(priority = 1)
+    void test1() {
+
+        Actions actions=new Actions(GenelWebDriver.driver);
+        SoftAssert sft=new SoftAssert();
+
+        POMCodes_112 elements = new POMCodes_112(driver);
+        elements.clickcokieaccept.click();
+
+        System.out.println("sayısı="+elements.counttheelements.size());
+
+
+        actions.moveToElement(elements.Allimagines).build().perform();
+
+        for (int i = 0; i <elements.counttheelements.size(); i++) {
+            scrollToIntoMiddle(elements.counttheelements.get(i));
+            wait.until(ExpectedConditions.elementToBeClickable(elements.counttheelements.get(i)));
+//            wait.until(ExpectedConditions.visibilityOf(elements.counttheelements.get(i)));
+            actions.moveToElement(elements.counttheelements.get(i)).build().perform();
+            Assert.assertTrue(elements.Allimagines.getText().contains(elements.counttheelements.get(i).getText()));
+            System.out.println(elements.counttheelements.get(i).getText());
+
+        }
+        System.out.println("the test passed");
+
+
+
+
 
 
     }
+    public void scrollToIntoMiddle(WebElement element) {
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String scrollElementIntoMiddle = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);"
+                + "var elementTop = arguments[0].getBoundingClientRect().top;"
+                + "window.scrollBy(0, elementTop-(viewPortHeight/2));";
+        js.executeScript(scrollElementIntoMiddle, element);
+
+    }
+}
+
+
+
 
